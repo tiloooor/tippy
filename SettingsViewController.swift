8bed0d78c91ab22af983e81d8b8684054f0f1970
Tyler Holloway <htoyll@fb.com>
2017-06-20 20:50:04 -0700

    added settings page

---
+++ SettingsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SettingsViewController.swift
+//  Tippy
+//
+//  Created by Tyler Holloway on 6/20/17.
+//  Copyright © 2017 Tyler Holloway. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.title = "Tip Calculator"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+ 
+    @IBAction func labelSliderChanged(_ sender: UISlider) {
+        
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
